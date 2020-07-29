00af4511220b666ff3b01ad4c67ae0c15ba8de7d
Cawan <cawan.porto@gmail.com>
2020-07-28 22:27:41 -0300

    fix output

--- modules/scheduler/output.tf
+++ modules/scheduler/output.tf
@@ -1,6 +1,6 @@
 output "aws_lambda_function_start" {
     value = var.schedule_start == true ? aws_lambda_function.start[0].arn  : 0 
-
+}
 output "aws_lambda_function_stop" {
     value = var.schedule_stop == true ? aws_lambda_function.stop[0].arn : 0
 }
